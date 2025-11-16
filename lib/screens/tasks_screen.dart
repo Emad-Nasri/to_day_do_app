452f2b0f1fb1974c13764a33bfd33bd4e9e9dcc0
emadnasri24 <emadnasri50@gmail.com>
2025-11-16 23:11:18 +0300

    create initial task screen

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        onPressed: null,
+        backgroundColor: Colors.indigo[400],
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      backgroundColor: Colors.teal[400],
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 20,
+          right: 20,
+          bottom: 80,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.playlist_add_check, size: 40, color: Colors.white),
+                SizedBox(width: 20),
+                Text(
+                  'ToDayDo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '4 Tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
