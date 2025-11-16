bb4e58f48bac57a8d64b1b74f0d73812b179822d
emadnasri24 <emadnasri50@gmail.com>
2025-11-17 00:33:03 +0300

    make tasks list statefull widget and create task model

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:to_day_do_app/models/task.dart';
 import 'package:to_day_do_app/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [];
   @override
   Widget build(BuildContext context) {
     return ListView(children: [TaskTile(), TaskTile(), TaskTile()]);
